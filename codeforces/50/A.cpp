72563bb046c0528dcc4c0110b988134b9ef88628
Yash Gupta <y25.gupta@gmail.com>
2018-12-16 19:28:00 +0530

    Add solution for problem `A - Domino piling` Link: https://codeforces.com/contest/50/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/50/submission/47107970

---
+++ codeforces/50/A.cpp
@@ -0,0 +1,40 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+
+const int N = 2 * 1e5 + 10;
+
+ll res;
+ll a[N];
+
+
+void solve()
+{
+	int m,n;
+	cin>>m>>n;
+	int x,y;
+	x=m*n;
+	y=2;
+	int ans;
+	ans=x/2;
+	cout<<ans;
+	
+	
+}
+
+int main(int argc, char const *argv[]){
+	ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+	#ifndef ONLINE_JUDGE
+    
+   	freopen("input.txt", "r", stdin);
+    
+	freopen("output.txt", "w", stdout);
+	#endif
+	ll t = 1;
+	//cin >> t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
