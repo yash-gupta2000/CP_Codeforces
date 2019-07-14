526a4c9a2aaa3bfaaa6110367fb6f3227d7441ce
Yash Gupta <y25.gupta@gmail.com>
2019-07-14 22:34:00 +0530

    Add solution for problem `A - Remove a Progression` Link: https://codeforces.com/contest/1194/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1194/submission/57058774

---
+++ codeforces/1194/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int q;cin>>q;
+    while(q--)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<2*b<<endl;
+    }
+    
+}
