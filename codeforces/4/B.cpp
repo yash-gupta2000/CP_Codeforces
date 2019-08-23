146b23a064c8a59f6df6d9c3d3ddbb6769b46387
Yash Gupta <y25.gupta@gmail.com>
2019-08-24 02:10:00 +0530

    Add solution for problem `B - Before an Exam` Link: https://codeforces.com/contest/4/problem/B Tags: constructive algorithms, greedy, *1200 Ref: https://codeforces.com/contest/4/submission/59374210

---
+++ codeforces/4/B.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+int main()
+{
+    ll i,j,n,time;
+    ll a[35],b[35];
+    while(cin>>n>>time){
+         ll min_time=0,max_time=0,sum_time=0;
+         vector<ll>v;
+        for(i=0;i<n;i++){
+            cin>>a[i]>>b[i];
+            min_time+=a[i];
+            max_time+=b[i];
+            v.push_back(a[i]);
+            sum_time+=a[i];
+        }
+        if(min_time<=time && time<=max_time){
+            cout<<"YES\n";
+            if(sum_time<time){
+            for(i=0;i<n;i++){
+                    ll dummy=b[i]-a[i];
+                    j=1;
+                while(j<=dummy){
+                        if(sum_time==time)
+                        break;
+                    sum_time++;
+                    v[i]++;
+                    j++;
+                }
+            }
+        }
+        for(i=0;i<v.size();i++){
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+    }
+    else
+        cout<<"NO\n";
+}
+    return 0;
+}
