bee2e8a5b2a9002e638c50feee2019758ac42428
Yash Gupta <y25.gupta@gmail.com>
2019-08-28 20:54:00 +0530

    Add solution for problem `B - Buttons` Link: https://codeforces.com/contest/268/problem/B Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/268/submission/59624511

---
+++ codeforces/268/B.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin>>n;
+  int sum=0;
+
+  for(int i=1;i<=n;i++)
+  {
+    sum += (n-i)*i;
+    
+   
+
+    
+  }
+  cout<<sum+n<<endl;
+
+}
