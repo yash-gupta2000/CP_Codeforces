23890f81c5712be55d82c1d31c5b0890f68d1e29
Yash Gupta <y25.gupta@gmail.com>
2019-08-30 21:54:00 +0530

    Add solution for problem `C - Book Reading` Link: https://codeforces.com/contest/1213/problem/C Tags: math, *1200 Ref: https://codeforces.com/contest/1213/submission/59754887

--- codeforces/1213/C.cpp
+++ codeforces/1213/C.cpp
@@ -1,59 +1,44 @@
-/*
-author: Yash Gupta
-*/
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define int long long
-#define all(x) x.begin(),x.end()
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-#define mem(x,y) memset(x,y,sizeof(x))
-#define pb push_back
-#define mp make_pair
-#define fir first
-#define sec second
-const int N = 2e5 + 5;
-const int inf = 9e18 + 9;
-const int mod = 1e9+7;
-vector<int> a;
+typedef long long ll;
 
-void solve()
+int main()
 {
-	 int n,m;
-	 cin>>n>>m;
-	 int quotient = n/m;
-	 int cycles = quotient/10;
-	 int extra = quotient%10;
-	 vector<int> digits;
-	 int cnt=1;
-	 int num = m;
-	 while(cnt!=11){
-		 num = m*cnt;
-		 digits.pb(num%10);
-		 cnt++;
-	 }
-	 int cycle_sum = 0;
-	 for(auto it:digits){
-		 cycle_sum += it;
-	 }
-	 int res = cycle_sum*cycles;
-	 for(auto it:digits){
-		 if(extra == 0){
-			 break;
-		 }
-		 res += it;
-		 extra--;
-	 }
-	 cout<<res<<endl;
-
-}
- 
-int32_t main()
-{
-	IOS;int t = 1;
-	cin>>t;
-	for(int i=1;i<=t;i++)
-	{
-		solve();
-	}
+  ll q;cin>>q;
+  while(q--)
+  {
+    ll n;
+    cin>>n;
+    ll m;
+    cin>>m;
+    vector<ll> v;
+    map<ll, ll> mp;
+    ll mm=m;
+    for(int i=0;i<10;i++)
+    {
+    	if(mp[mm%10])
+    		break;
+    	v.push_back(mm%10);
+    	mm = mm+m;
+    	// cout<<v[i]<<endl;
+    }
+    ll a,b,c;
+    a = n/m;
+    //cout<<"A"<<" "<<a<<endl;
+    b = a / v.size();
+    c = a % v.size();
+    //cout<<"b"<<b<<" "<<"c"<<c<<endl;
+    ll sum1 = 0;
+    for(auto i : v){
+    	sum1 += i;
+    }
+    ll ans = b * sum1;
+    	for(int i=0;i<c;i++)
+    	{
+    		ans += v[i];
+    	}
+    
+    cout<<ans<<endl;
+    
+  }
 }
