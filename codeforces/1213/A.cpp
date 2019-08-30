3d958782fdc11a462ceecafbc4cd0f388da48335
Yash Gupta <y25.gupta@gmail.com>
2019-08-30 20:16:00 +0530

    Add solution for problem `A - Chips Moving` Link: https://codeforces.com/contest/1213/problem/A Tags: math, *900 Ref: https://codeforces.com/contest/1213/submission/59717381

--- codeforces/1213/A.cpp
+++ codeforces/1213/A.cpp
@@ -1,47 +1,37 @@
-/*
-author: Yash Gupta
-*/
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define int long long
-#define all(x) x.begin(),x.end()
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-#define mem(x,y) memset(x,y,sizeof(x))
-#define pb push_back
-#define mp make_pair
-#define fir first
-#define sec second
-const int N = 2e5 + 5;
-const int inf = 9e18 + 9;
-const int mod = 1e9+7;
-vector<int> a;
 
-void solve()
+int main()
 {
-	 int n;cin>>n;
-	 vector<int> a(n);
-	 int odd=0,even=0;
-	 for(int i=0;i<n;i++){
-		 cin>>a[i];
-		 if(a[i]%2){
-			 odd++;
-		 }
-		 else{
-			 even++;
-		 }
-	 }
-	 cout<<min(odd,even)<<endl;
-	 
-	 
-}
- 
-int32_t main()
-{
-	IOS;int t = 1;
-	//cin>>t;
-	for(int i=1;i<=t;i++)
-	{
-		solve();
-	}
+  int n;
+  cin>>n;
+  int a[n];
+  int even=0,odd=0;
+  int ans=0;
+  for(int i=0;i<n;i++)
+  {
+    cin>>a[i];
+    if(a[i]%2==0)
+    {
+      even++;
+    }
+    else
+    {
+      odd++;
+    }
+  }
+  if(odd>even)
+  {
+    ans=even;
+  }
+  else if(even>odd)
+  {
+    ans = odd;
+  }
+  else
+  {
+    ans = even;
+  }
+  cout<<ans;
+  
 }
