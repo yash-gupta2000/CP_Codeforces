99a9f3b3e649e8631b5bbe0c24a17f6a181cf0f1
Yash Gupta <y25.gupta@gmail.com>
2019-09-01 00:24:00 +0530

    Add solution for problem `D1 - Equalizing by Division (easy version)` Link: https://codeforces.com/contest/1213/problem/D1 Tags: brute force, implementation, *1500 Ref: https://codeforces.com/contest/1213/submission/59824701

--- codeforces/1213/D1.cpp
+++ codeforces/1213/D1.cpp
@@ -1,63 +1,42 @@
-/*
-author: Yash Gupta
-*/
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define int long long
-#define all(x) x.begin(),x.end()
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-#define mem(x,y) memset(x,y,sizeof(x))
-#define pb push_back
-#define mp make_pair
-#define fir first
-#define sec second
-const int N = 2e5 + 5;
-const int inf = 9e18 + 9;
-const int mod = 1e9+7;
-vector<int> a;
+typedef long long ll;
 
-void solve()
+int main()
 {
-	 int n,k;cin>>n>>k;
-	 vector<int> a(n);
-	 for(int i=0;i<n;i++){
-		 cin>>a[i];
-	 }
-	 map<int,vector<int>> m;
-	 for(int i=0;i<n;i++){
-		 //hrr element ke brabar bnane m operations
-		 int elmt = a[i];
-		 int operations = 0;
-		 while(elmt){
-			 m[elmt].pb(operations);
-			 elmt /= 2;
-			 operations++;
-		 }
-	 }
-	 int res = INT_MAX;
-	 for(auto it:m){
-		 sort(all(it.second));
-		 if(it.second.size() < k){
-			 continue;
-		 }
-		 else{
-			 int sum = 0;
-			 for(int i=0;i<k;i++){
-				 sum += it.second[i];
-			 }
-			 res = min(res,sum);
-		 }
-	 }
-	 cout<<res<<endl;
-}
- 
-int32_t main()
-{
-	IOS;int t = 1;
-	// cin>>t;
-	for(int i=1;i<=t;i++)
-	{
-		solve();
+    ll n,k;
+    cin>>n>>k;
+    vector<int> v;
+    for(int i=0;i<n;i++)
+    {
+        int input;
+        cin>>input;
+        v.push_back(input);
+    }
+
+    vector<vector<int>> vi(2000*100+11);
+    for(int i=0;i<n;i++)
+    {
+        int x = v[i];
+        int cur=0;
+        while(x>0)
+        {
+            vi[x].push_back(cur);
+            x /= 2;
+            cur++;
+        }
+
+    }
+    int ans = 1e9;
+	for (int i = 0; i <= 200 * 1000; ++i) 
+    {
+		sort(vi[i].begin(), vi[i].end());
+		if (int(vi[i].size()) < k) continue;
+		ans = min(ans, accumulate(vi[i].begin(), vi[i].begin() + k, 0));
 	}
+	
+	cout << ans << endl;
+	
+	return 0;
+    
 }
