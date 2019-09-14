43dd8610940591bee853dfe642384826085ee1ba
Yash Gupta <y25.gupta@gmail.com>
2019-09-15 00:46:00 +0530

    Add solution for problem `C - Bad Sequence` Link: https://codeforces.com/contest/1214/problem/C Tags: data structures, greedy, *1200 Ref: https://codeforces.com/contest/1214/submission/60582696

--- codeforces/1214/C.cpp
+++ codeforces/1214/C.cpp
@@ -6,25 +6,43 @@ int main()
 {
     int n;
     cin>>n;
-    string s;cin>>s;
-    int med=0;
-    for(auto &it:s)
+    vector<char> v(n);
+    int cntx=0,cnty=0;
+    for(auto &it:v)
     {
+        cin>>it;
         if(it=='(')
         {
-            med++;
+            cntx++;
         }
         else
         {
-            med--;
-        }
-        if(med<-1)
-        {
-            cout<<"No"<<endl;
-            return 0;
-        }   
+            cnty++;
+        }  
     }
-    if(med==0)
+    if(cntx!=cnty)
+    {
+        cout<<"NO"<<endl;
+        return 0;
+    }
+    stack<char> s;
+
+    for(int i=0;i<n;i++)
+    {
+        if(s.empty()||s.top()==v[i])
+        {
+            s.push(v[i]);
+        }
+        else if(s.top()=='(' && v[i]==')')
+        {
+            s.pop();
+        }
+        else
+        {
+            s.push(v[i]);
+        }
+    }
+    if(s.size()==2 ||s.size()==0)
     {
         cout<<"Yes"<<endl;
     }
@@ -32,7 +50,5 @@ int main()
     {
         cout<<"No"<<endl;
     }
-    
-    
     
 }
