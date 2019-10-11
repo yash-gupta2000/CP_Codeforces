e703e2d8b2fc4287ca44c8183a89f8f66b87bf33
Yash Gupta <y25.gupta@gmail.com>
2019-10-12 00:02:00 +0530

    Add solution for problem `C - Dividing the numbers` Link: https://codeforces.com/contest/899/problem/C Tags: constructive algorithms, graphs, math, *1300 Ref: https://codeforces.com/contest/899/submission/62379442

---
+++ codeforces/899/C.cpp
@@ -0,0 +1,84 @@
+ 
+#include<iostream>
+#include<algorithm>
+#include<bitset>
+ 
+#include<cmath>
+#include<cstring>
+#include<climits>
+ 
+#include<deque>
+#include<queue>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+ 
+#include<fstream>
+#include<chrono>
+using namespace std;
+ 
+typedef long long LL;
+typedef pair<LL,LL> p64;
+typedef vector<LL> v64;
+typedef map<LL,LL> mp64;
+ 
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define rep(i,s,e) for(long long i=s;i<=e;i++)
+#define brep(i,s,e) for(long long i=s;i>=e;i--)
+#define all(x) x.begin(),x.end()
+#define mem(x,y) memset(x,y,sizeof(x))
+#define DANGER std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+ 
+void solve()
+{
+    LL n;cin>>n;
+    v64 v;
+    LL sum = n*(n+1)/2;
+    if(sum&1)
+    {
+        cout<<"1"<<endl;
+    }
+    else
+    {
+        cout<<"0"<<endl;
+    }
+    sum /= 2;
+    for(int i=n;i>0;i--)
+    {
+        if(sum-i>=0)
+        {
+            v.pb(i);
+            sum -= i;
+        }
+    }
+    cout<<v.size()<<" ";
+    for(auto it:v)
+    {
+        cout<<it<<" ";
+    }
+    
+}
+ 
+int main()
+{
+    DANGER;
+    ofstream out("output.txt");
+ 
+    auto start = chrono::high_resolution_clock::now();
+    
+    LL t = 1;
+    // cin>>t;
+    rep(test,1,t)
+    {
+        solve();
+    }
+    
+    auto stop = chrono::high_resolution_clock::now();
+    // auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    // cout << "Time taken by function: " << duration.count() << " milliseconds" << endl;
+}
