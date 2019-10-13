3cf476b1a05268b72b19fe0e10acfb31dd032ab7
Yash Gupta <y25.gupta@gmail.com>
2019-10-13 23:01:00 +0530

    Add solution for problem `A - Pens and Pencils` Link: https://codeforces.com/contest/1244/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1244/submission/62526379

---
+++ codeforces/1244/A.cpp
@@ -0,0 +1,74 @@
+ 
+#include<iostream>
+#include<algorithm>
+#include<bitset>
+ 
+#include<cmath>
+#include<cstring>
+#include<climits>
+ 
+#include<deque>
+#include<queue>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+ 
+#include<fstream>
+#include<chrono>
+using namespace std;
+ 
+typedef long long LL;
+typedef pair<LL,LL> p64;
+typedef vector<LL> v64;
+typedef map<LL,LL> mp64;
+ 
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define rep(i,s,e) for(long long i=s;i<=e;i++)
+#define brep(i,s,e) for(long long i=s;i>=e;i--)
+#define all(x) x.begin(),x.end()
+#define mem(x,y) memset(x,y,sizeof(x))
+#define DANGER std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+ 
+void solve()
+{
+     LL a,b,c,d,k;
+   	 cin>>a>>b>>c>>d>>k;
+   	 LL f=0;
+   	 LL x,y;
+   	 for(LL i=0;i<=k;i++)
+     {
+         if(i*c>=a and (k-i)*d>=b)
+         {
+             f=1,x=i,y=k-i;
+         }
+     }
+	 if(f)
+     {
+         cout<<x<<" "<<y<<endl;
+     }
+	 else cout<<-1<<endl;
+}
+ 
+int main()
+{
+    DANGER;
+    ofstream out("output.txt");
+ 
+    auto start = chrono::high_resolution_clock::now();
+    
+    LL t = 1;
+    cin>>t;
+    rep(test,1,t)
+    {
+        solve();
+    }
+    
+    auto stop = chrono::high_resolution_clock::now();
+    // auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    // cout << "Time taken by function: " << duration.count() << " milliseconds" << endl;
+}
