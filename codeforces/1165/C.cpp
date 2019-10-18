89323ea5d6d9c24ac20f22a299368748fcdd187d
Yash Gupta <y25.gupta@gmail.com>
2019-10-18 22:28:00 +0530

    Add solution for problem `C - Good String` Link: https://codeforces.com/contest/1165/problem/C Tags: greedy, *1300 Ref: https://codeforces.com/contest/1165/submission/62881715

---
+++ codeforces/1165/C.cpp
@@ -0,0 +1,75 @@
+ 
+#include<iostream>
+#include<algorithm>
+#include<bitset>
+ 
+#include<cmath>
+#include<cstring>
+#include<climits>
+ 
+#include<deque>
+#include<queue>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+ 
+#include<fstream>
+#include<chrono>
+using namespace std;
+ 
+typedef long long LL;
+typedef pair<LL,LL> p64;
+typedef vector<LL> v64;
+typedef map<LL,LL> mp64;
+ 
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define rep(i,s,e) for(long long i=s;i<=e;i++)
+#define brep(i,s,e) for(long long i=s;i>=e;i--)
+#define all(x) x.begin(),x.end()
+#define mem(x,y) memset(x,y,sizeof(x))
+#define DANGER std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+ 
+void solve()
+{
+     int n;cin>>n;
+     string s;cin>>s;
+     string t="";
+     for(int i=0;i<n;i++)
+     {
+         if(s[i]!=t.back() || t.length()%2==0)
+         {
+             t += s[i];
+         }
+     }
+     if(t.length()%2)
+     {
+         t.pop_back();
+     }
+     cout<<n-t.length()<<endl;
+     cout<<t<<endl;
+     return;
+}
+ 
+int main()
+{
+    DANGER;
+    ofstream out("output.txt");
+ 
+    auto start = chrono::high_resolution_clock::now();
+    
+    LL t = 1;
+    // cin>>t;
+    rep(test,1,t)
+    {
+        solve();
+    }
+    
+    auto stop = chrono::high_resolution_clock::now();
+    // auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    // cout << "Time taken by function: " << duration.count() << " milliseconds" << endl;
+}
