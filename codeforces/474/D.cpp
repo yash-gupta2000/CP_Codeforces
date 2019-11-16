d6d45abacbbb6d1ee03a2f2438cd9543443dc258
Yash Gupta <y25.gupta@gmail.com>
2019-11-16 13:43:00 +0530

    Add solution for problem `D - Flowers` Link: https://codeforces.com/contest/474/problem/D Tags: dp, *1700 Ref: https://codeforces.com/contest/474/submission/65152875

--- codeforces/474/D.cpp
+++ codeforces/474/D.cpp
@@ -1,60 +1,87 @@
-/*
-author: Yash Gupta
-*/
-#include <bits/stdc++.h>
+ 
+#include<iostream>
+#include<algorithm>
+#include<bitset>
+ 
+#include<cmath>
+#include<cstring>
+#include<climits>
+ 
+#include<deque>
+#include<queue>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+ 
+#include<fstream>
+#include<chrono>
 using namespace std;
-#define int long long
-#define all(x) x.begin(),x.end()
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-#define mem(x,y) memset(x,y,sizeof(x))
+ 
+typedef long long LL;
+typedef pair<LL,LL> p64;
+typedef vector<LL> v64;
+typedef map<LL,LL> mp64;
+ 
 #define pb push_back
 #define mp make_pair
-#define fir first
-#define sec second
-const int N = 1e5;
-const int inf = 9e18 + 9;
-const int mod = 1e9+7;
-vector<int> a;
-inline int add(int a, int b)
-{
-    return a + b >= mod ? a + b - mod : a + b;
-}
-inline int sub(int a, int b)
-{
-    return a - b < 0 ? a - b + mod : a - b;
-}
-int dp[100500];
+#define ff first
+#define ss second
+#define rep(i,s,e) for(long long i=s;i<=e;i++)
+#define brep(i,s,e) for(long long i=s;i>=e;i--)
+#define all(x) x.begin(),x.end()
+#define mem(x,y) memset(x,y,sizeof(x))
+#define DANGER std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+LL N = 1e5+9;
+LL h[100000+45];
+LL mod = 1e9 + 7;
 void solve()
 {
-	 int t,k;
-    cin >> t >> k;
-    for (int i=0;i<k;i++)
-    {
-        dp[i]=1;
-    }
-    for (int i=k;i<=N;i++)
-    {
-        dp[i]=add(dp[i-1],dp[i-k]);
-    }
-    for (int i=1;i<=N;i++)
-    {
-        dp[i]=add(dp[i-1],dp[i]);
-    }
-    while(t--)
-    {
-        int a,b;
-        cin >> a >> b;
-        cout << sub(dp[b],dp[a-1]) << endl;;
-    }
+   LL t, k, l, r;
+   cin >> t >> k;
+   h[0]  =1;
+   for(int i=1;i<=k-1;i++)
+   {
+       h[i] = 1;
+
+   }
+   h[k] = 2;
+   for(int i=k+1;i<=N;i++) {
+       h[i] = (h[i - k] + h[i - 1]) % mod;
+   }
+// //    for(int i=0;i<=10;i++)
+//         cout << h[i] << ' ';
+    
+   for(int i=1;i<=N-1;i++)
+   {
+       h[i] = (h[i] + h[i-1]) % mod;
+   }
+       
+       
+   while(t--) {
+     cin >> l >> r;
+     //cout<<h[r]<<" "<<h[l-1]<<endl;
+     cout << (h[r] - h[l-1]+mod) % mod << '\n';            
+   } 
+    return ;
 }
  
-int32_t main()
+int main()
 {
-	IOS;int t = 1;
-	// cin>>t;
-	for(int i=1;i<=t;i++)
-	{
-		solve();
-	}
+    DANGER;
+    ofstream out("output.txt");
+ 
+    auto start = chrono::high_resolution_clock::now();
+    
+    LL t = 1;
+    // cin>>t;
+    rep(test,1,t)
+    {
+        solve();
+    }
+    
+    auto stop = chrono::high_resolution_clock::now();
+    // auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    // cout << "Time taken by function: " << duration.count() << " milliseconds" << endl;
 }
