0d754442faf5edabcda64ecaf8034111b67c4103
Yash Gupta <y25.gupta@gmail.com>
2020-02-25 19:13:00 +0530

    Add solution for problem `B - Cow and Friend` Link: https://codeforces.com/contest/1307/problem/B Tags: geometry, greedy, math, *1300 Ref: https://codeforces.com/contest/1307/submission/71861548

---
+++ codeforces/1307/B.cpp
@@ -0,0 +1,85 @@
+ 
+//It doesn't matter how slow you go, Unless you don't stop.
+#include<iostream>
+#include<algorithm>
+#include<bitset>
+ 
+#include<cmath>
+#include<cstring>
+#include<climits>
+ 
+#include<deque>
+#include<queue>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+ 
+#include<fstream>
+#include<chrono>
+using namespace std;
+ 
+typedef long long LL;
+typedef pair<LL,LL> p64;
+typedef vector<LL> v64;
+typedef map<LL,LL> mp64;
+ 
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define rep(i,s,e) for(long long i=s;i<=e;i++)
+#define brep(i,s,e) for(long long i=s;i>=e;i--)
+#define all(x) x.begin(),x.end()
+#define mem(x,y) memset(x,y,sizeof(x))
+#define DANGER std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+ 
+void solve()
+{
+     LL n,dist;
+     cin>>n>>dist;
+     set<LL> s;
+     LL mx = INT_MIN;
+     for(int i=0;i<n;i++)
+     {
+         LL x;cin>>x;
+         s.insert(x);
+         if(x>mx)
+         {
+             mx = x;
+         }
+     }
+     if(s.find(dist)!=s.end())
+     {
+         cout<<"1"<<endl;
+     }
+     else
+     {
+         LL res = max(2LL,((dist+mx-1)/mx));
+         cout<<res<<endl;
+     }
+     
+     
+
+     
+}
+ 
+int main()
+{
+    DANGER;
+    ofstream out("output.txt");
+ 
+    auto start = chrono::high_resolution_clock::now();
+    
+    LL t = 1;
+    cin>>t;
+    rep(test,1,t)
+    {
+        solve();
+    }
+    
+    auto stop = chrono::high_resolution_clock::now();
+    // auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    // cout << "Time taken by function: " << duration.count() << " milliseconds" << endl;
+}
