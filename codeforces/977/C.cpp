abc76c49a4a6dc3fa5f8ec2c9002c582bee50762
Yash Gupta <y25.gupta@gmail.com>
2020-04-26 11:48:00 +0530

    Add solution for problem `C - Less or Equal` Link: https://codeforces.com/contest/977/problem/C Tags: sortings, *1200 Ref: https://codeforces.com/contest/977/submission/78088089

---
+++ codeforces/977/C.cpp
@@ -0,0 +1,91 @@
+ 
+//It doesn't matter how slow you go, Unless you don't stop.
+#include<iostream>
+#include<algorithm>
+#include<bitset>
+ 
+#include<cmath>
+#include<cstring>
+#include<climits>
+ 
+#include<deque>
+#include<queue>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+ 
+#include<fstream>
+#include<chrono>
+using namespace std;
+ 
+typedef long long LL;
+typedef pair<LL,LL> p64;
+typedef vector<LL> v64;
+typedef map<LL,LL> mp64;
+ 
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define rep(i,s,e) for(long long i=s;i<=e;i++)
+#define brep(i,s,e) for(long long i=s;i>=e;i--)
+#define all(x) x.begin(),x.end()
+#define mem(x,y) memset(x,y,sizeof(x))
+#define DANGER std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+ 
+void solve()
+{
+     int n,k;
+     cin>>n>>k;
+     int a[n];
+     
+     for(int i=0;i<n;i++)
+     {
+         cin>>a[i];
+     }
+     sort(a,a+n);
+     if(k==0 && a[0]==1)
+     {
+         cout<<"-1"<<endl;
+         return;
+     }
+     else if(k==0 && a[0]>1)
+     {
+         cout<<"1"<<endl;
+         return;
+     }
+     int ans=0;
+     int elmt = a[k-1];
+     if(a[k]==elmt)
+     {
+         cout<<"-1"<<endl;
+         return;
+     }
+     else
+     {
+         cout<<elmt<<endl;
+     }
+     
+
+}
+ 
+int main()
+{
+    DANGER;
+    ofstream out("output.txt");
+ 
+    auto start = chrono::high_resolution_clock::now();
+    
+    LL t = 1;
+    // cin>>t;
+    rep(test,1,t)
+    {
+        solve();
+    }
+    
+    auto stop = chrono::high_resolution_clock::now();
+    // auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    // cout << "Time taken by function: " << duration.count() << " milliseconds" << endl;
+}
