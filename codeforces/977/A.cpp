a59668519649985d07a62e63aac4c0bb83b51f65
Yash Gupta <y25.gupta@gmail.com>
2020-04-26 11:19:00 +0530

    Add solution for problem `A - Wrong Subtraction` Link: https://codeforces.com/contest/977/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/977/submission/78086181

--- codeforces/977/A.cpp
+++ codeforces/977/A.cpp
@@ -1,32 +1,53 @@
-/*
-author: Yash Gupta
-*/
-#include <bits/stdc++.h>
+ 
+//It doesn't matter how slow you go, Unless you don't stop.
+#include<iostream>
+#include<algorithm>
+#include<bitset>
+ 
+#include<cmath>
+#include<cstring>
+#include<climits>
+ 
+#include<deque>
+#include<queue>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+ 
+#include<fstream>
+#include<chrono>
 using namespace std;
-typedef long long ll;
-#define all(x) x.begin(),x.end()
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-#define mem(x,y) memset(x,y,sizeof(x))
+ 
+typedef long long LL;
+typedef pair<LL,LL> p64;
+typedef vector<LL> v64;
+typedef map<LL,LL> mp64;
+ 
 #define pb push_back
 #define mp make_pair
-#define fir first
-#define sec second
-const int N = 2e5 + 5;
-const ll inf = 9e18 + 9;
-const int mod = 1e9+7;
-ll res;
-
+#define ff first
+#define ss second
+#define rep(i,s,e) for(long long i=s;i<=e;i++)
+#define brep(i,s,e) for(long long i=s;i>=e;i--)
+#define all(x) x.begin(),x.end()
+#define mem(x,y) memset(x,y,sizeof(x))
+#define DANGER std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+ 
 void solve()
 {
-     int n,k;cin>>n>>k;
-     while(k--){
-         int last = n%10;
-         if(last==0){
-             n = n/10;
-         }
-         else{
+     LL n,k;
+     cin>>n>>k;
+     while(k--)
+     {
+         if(n%10)
+         {
              n--;
+         }
+         else
+         {
+             n = n/10;
          }
      }
      cout<<n<<endl;
@@ -34,10 +55,19 @@ void solve()
  
 int main()
 {
-    IOS;ll t = 1;
+    DANGER;
+    ofstream out("output.txt");
+ 
+    auto start = chrono::high_resolution_clock::now();
+    
+    LL t = 1;
     // cin>>t;
-    for(int i=1;i<=t;i++)
+    rep(test,1,t)
     {
         solve();
     }
+    
+    auto stop = chrono::high_resolution_clock::now();
+    // auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    // cout << "Time taken by function: " << duration.count() << " milliseconds" << endl;
 }
