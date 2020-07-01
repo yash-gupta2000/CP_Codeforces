6d5e27af62c89d2d5e60021278e9609bfe4e1bdc
Yash Gupta <y25.gupta@gmail.com>
2020-07-01 20:06:00 +0530

    Add solution for problem `B - Magical Calendar` Link: https://codeforces.com/contest/1371/problem/B Tags: math, *1200 Ref: https://codeforces.com/contest/1371/submission/85666263

---
+++ codeforces/1371/B.cpp
@@ -0,0 +1,74 @@
+ 
+//It doesn't matter how slow you go, Unless you don't stop.
+#include<iostream>
+#include<algorithm>
+#include<bitset>
+ 
+#include<cmath>
+#include<cstring>
+#include<climits>
+ 
+#include<deque>
+#include<queue>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+ 
+#include<fstream>
+#include<chrono>
+using namespace std;
+ 
+typedef long long LL;
+typedef pair<LL,LL> p64;
+typedef vector<LL> v64;
+typedef map<LL,LL> mp64;
+ 
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define rep(i,s,e) for(long long i=s;i<=e;i++)
+#define brep(i,s,e) for(long long i=s;i>=e;i--)
+#define all(x) x.begin(),x.end()
+#define mem(x,y) memset(x,y,sizeof(x))
+#define DANGER std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+ 
+void solve()
+{
+     LL n,r;cin>>n>>r;
+     LL dummy;
+     if(n > r){
+         dummy = r;
+     }
+     else{
+         dummy = n;
+     }
+     if(n<=r){
+         LL elmt = ((n-1)*n)/2;
+         elmt++;
+         cout<<elmt<<endl;
+     }else{
+         cout<<((r)*(r+1))/2<<endl;
+     }
+}
+ 
+int main()
+{
+    DANGER;
+    ofstream out("output.txt");
+ 
+    auto start = chrono::high_resolution_clock::now();
+    
+    LL t = 1;
+    cin>>t;
+    rep(test,1,t)
+    {
+        solve();
+    }
+    
+    auto stop = chrono::high_resolution_clock::now();
+    // auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    // cout << "Time taken by function: " << duration.count() << " milliseconds" << endl;
+}
