1923bddaeff1e8a24d3f62daf20ee0f8a74f71af
Yash Gupta <y25.gupta@gmail.com>
2020-09-04 22:18:00 +0530

    Add solution for problem `B - Minimum Product` Link: https://codeforces.com/contest/1409/problem/B Tags: brute force, greedy, math, *1100 Ref: https://codeforces.com/contest/1409/submission/91878280

---
+++ codeforces/1409/B.cpp
@@ -0,0 +1,74 @@
+ 
+//It doesn't matter how slow you go, Unless you don't stop.
+#include<iostream>
+#include<algorithm>
+#include<bitset>
+ 
+#include<cmath>
+#include<cstring>
+#include<climits>
+ 
+#include<deque>
+#include<queue>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+ 
+#include<fstream>
+#include<chrono>
+using namespace std;
+ 
+typedef long long LL;
+typedef pair<LL,LL> p64;
+typedef vector<LL> v64;
+typedef map<LL,LL> mp64;
+ 
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define rep(i,s,e) for(long long i=s;i<=e;i++)
+#define brep(i,s,e) for(long long i=s;i>=e;i--)
+#define all(x) x.begin(),x.end()
+#define mem(x,y) memset(x,y,sizeof(x))
+#define DANGER std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+ 
+void solve()
+{
+     LL a,b,x,y,moves;
+     cin>>a>>b>>x>>y>>moves;
+     LL mina = max(x,a-moves);
+     LL minb = max(y,b-moves);
+     if(mina>minb){
+         moves -= (b-minb);
+         a = max(x,a-moves);
+         cout<<a*minb<<endl;
+     }
+     else{
+         moves -= (a-mina);
+         b = max(y,b-moves);
+         cout<<b*mina<<endl;
+     }
+     
+}
+ 
+int main()
+{
+    DANGER;
+    ofstream out("output.txt");
+ 
+    auto start = chrono::high_resolution_clock::now();
+    
+    LL t = 1;
+    cin>>t;
+    rep(test,1,t)
+    {
+        solve();
+    }
+    
+    auto stop = chrono::high_resolution_clock::now();
+    // auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    // cout << "Time taken by function: " << duration.count() << " milliseconds" << endl;
+}
