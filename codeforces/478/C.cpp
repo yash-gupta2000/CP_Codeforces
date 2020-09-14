33bef51b1e23e7dbe16bf67493785377e47a729a
Yash Gupta <y25.gupta@gmail.com>
2020-09-14 15:58:00 +0530

    Add solution for problem `C - Table Decorations` Link: https://codeforces.com/contest/478/problem/C Tags: greedy, *1800 Ref: https://codeforces.com/contest/478/submission/92779077

---
+++ codeforces/478/C.cpp
@@ -0,0 +1,70 @@
+ 
+//It doesn't matter how slow you go, Unless you don't stop.
+#include<iostream>
+#include<algorithm>
+#include<bitset>
+ 
+#include<cmath>
+#include<cstring>
+#include<climits>
+ 
+#include<deque>
+#include<queue>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+ 
+#include<fstream>
+#include<chrono>
+using namespace std;
+ 
+typedef long long LL;
+typedef pair<LL,LL> p64;
+typedef vector<LL> v64;
+typedef map<LL,LL> mp64;
+ 
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define rep(i,s,e) for(long long i=s;i<=e;i++)
+#define brep(i,s,e) for(long long i=s;i>=e;i--)
+#define all(x) x.begin(),x.end()
+#define mem(x,y) memset(x,y,sizeof(x))
+#define DANGER std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+ 
+void solve()
+{
+    vector<LL> v(3);
+    cin>>v[0];
+    cin>>v[1];
+    cin>>v[2]; 
+    sort(all(v));
+    LL sum = (v[0]+v[1]+v[2]); 
+    LL res = sum/3;
+    if(v[3] > 2*(v[0]+v[1])){
+        res = min(res,(LL)v[0]+v[1]);
+    }  
+    cout<<res<<endl;
+}
+ 
+int main()
+{
+    DANGER;
+    ofstream out("output.txt");
+ 
+    auto start = chrono::high_resolution_clock::now();
+    
+    LL t = 1;
+    // cin>>t;
+    rep(test,1,t)
+    {
+        solve();
+    }
+    
+    auto stop = chrono::high_resolution_clock::now();
+    // auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    // cout << "Time taken by function: " << duration.count() << " milliseconds" << endl;
+}
