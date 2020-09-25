76d63321d121560cb1f53fda9f6fabf7caa5b782
Yash Gupta <y25.gupta@gmail.com>
2020-09-25 15:54:00 +0530

    Add solution for problem `A - Rational Resistance` Link: https://codeforces.com/contest/343/problem/A Tags: math, number theory, *1600 Ref: https://codeforces.com/contest/343/submission/93773192

---
+++ codeforces/343/A.cpp
@@ -0,0 +1,43 @@
+//Currently in Spain, But S is silent :|
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) x.begin(),x.end()
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define mem(x,y) memset(x,y,sizeof(x))
+#define pb push_back
+#define mp make_pair
+#define fir first
+#define sec second
+const int N = 2e5 + 5;
+const ll inf = 9e18 + 9;
+const int mod = 1e9+7;
+ll res;
+
+void solve()
+{
+     long long a, b;
+  cin >> a >> b;
+  long long ans = 0;
+  while (a > 0 && b > 0) {
+    if (a > b) {
+      ans += a / b;
+      a %= b;
+    } else {
+      ans += b / a;
+      b %= a;
+    }
+  }	
+  cout << ans << endl;
+}
+ 
+int main()
+{
+    IOS;ll t = 1;
+    // cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        solve();
+    }
+}
