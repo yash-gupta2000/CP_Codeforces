311643d2960a4b7169b39da62d734a192526a500
Yash Gupta <y25.gupta@gmail.com>
2020-11-27 18:38:00 +0530

    Add solution for problem `A - Polycarp's Pockets` Link: https://codeforces.com/contest/1003/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1003/submission/99717750

---
+++ codeforces/1003/A.cpp
@@ -0,0 +1,45 @@
+/*
+author: Yash Gupta
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(x) x.begin(),x.end()
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define mem(x,y) memset(x,y,sizeof(x))
+#define pb push_back
+#define mp make_pair
+#define fir first
+#define sec second
+const int N = 2e5 + 5;
+const int inf = 9e18 + 9;
+const int mod = 1e9+7;
+vector<int> a;
+
+void solve()
+{
+	 int n;cin>>n;
+	 vector<int> a(n);
+	 map<int,int> m;
+	 for(int i=0;i<n;i++){
+		 cin>>a[i];
+		 m[a[i]]++;
+	 }
+	 int mx = -1;
+	 for(auto it:m){
+		 mx = max(it.second,mx);
+	 }
+	 cout<<mx<<endl;
+
+}
+ 
+int32_t main()
+{
+	IOS;int t = 1;
+	// cin>>t;
+	for(int i=1;i<=t;i++)
+	{
+		solve();
+	}
+}
