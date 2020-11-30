e642106b33e06438807b62ccb58aa67b974430ce
Yash Gupta <y25.gupta@gmail.com>
2020-11-30 12:15:00 +0530

    Add solution for problem `A - Adjacent Replacements` Link: https://codeforces.com/contest/1006/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1006/submission/99958422

---
+++ codeforces/1006/A.cpp
@@ -0,0 +1,46 @@
+/*
+author: Yash Gupta
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(x) x.begin(),x.end()
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define mem(x,y) memset(x,y,sizeof(x))
+#define pb push_back
+#define mp make_pair
+#define fir first
+#define sec second
+const int N = 2e5 + 5;
+const int inf = 9e18 + 9;
+const int mod = 1e9+7;
+vector<int> a;
+
+void solve()
+{
+	 int n;cin>>n;
+	 vector<int> a(n);
+	 for(int i=0;i<n;i++){
+		 cin>>a[i];
+	 }
+	 for(int i=0;i<n;i++){
+		 if(a[i]%2==0){
+			 a[i] -= 1;
+		 }
+	 }
+	 for(int i=0;i<n;i++){
+		 cout<<a[i]<<" ";
+	 }
+	 cout<<endl;
+}
+ 
+int32_t main()
+{
+	IOS;int t = 1;
+	//cin>>t;
+	for(int i=1;i<=t;i++)
+	{
+		solve();
+	}
+}
