3a0ee4cda47d14ffeeb2c8e2fcf5e11598db4f24
Yash Gupta <y25.gupta@gmail.com>
2020-12-01 15:08:00 +0530

    Add solution for problem `D - Taxes` Link: https://codeforces.com/contest/735/problem/D Tags: math, number theory, *1600 Ref: https://codeforces.com/contest/735/submission/100099442

---
+++ codeforces/735/D.cpp
@@ -0,0 +1,53 @@
+/*
+author: Yash Gupta
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(x) x.begin(),x.end()
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define mem(x,y) memset(x,y,sizeof(x))
+#define pb push_back
+#define mp make_pair
+#define fir first
+#define sec second
+const int N = 2e5 + 5;
+const int inf = 9e18 + 9;
+const int mod = 1e9+7;
+bool isPrime(int n) 
+{ 
+    // Corner cases 
+    if (n <= 1)  return false; 
+    if (n <= 3)  return true; 
+    if (n%2 == 0 || n%3 == 0) return false; 
+  
+    for (int i=5; i*i<=n; i=i+6) 
+        if (n%i == 0 || n%(i+2) == 0) 
+           return false; 
+  
+    return true; 
+}
+void solve()
+{
+	 int n;cin>>n;
+	 if(isPrime(n)){
+		 cout<<"1"<<endl;
+	 }
+	 else if(!isPrime(n-2) && n%2){
+		 cout<<3<<endl;
+	 }
+	 else{
+		 cout<<2<<endl;
+	 }
+}
+ 
+int32_t main()
+{
+	IOS;int t = 1;
+	// cin>>t;
+	for(int i=1;i<=t;i++)
+	{
+		solve();
+	}
+}
