33d48476681d975274928b0dee513a735a4ab6be
Yash Gupta <y25.gupta@gmail.com>
2020-12-05 13:13:00 +0530

    Add solution for problem `A - Diverse Strings` Link: https://codeforces.com/contest/1144/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1144/submission/100429002

---
+++ codeforces/1144/A.cpp
@@ -0,0 +1,42 @@
+/*
+author: Yash Gupta
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(x) x.begin(),x.end()
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define mem(x,y) memset(x,y,sizeof(x))
+#define pb push_back
+#define mp make_pair
+#define fir first
+#define sec second
+const int N = 2e5 + 5;
+const int inf = 9e18 + 9;
+const int mod = 1e9+7;
+vector<int> a;
+
+void solve()
+{
+	 string s;cin>>s;
+	 sort(all(s));
+	 for(int i=0;i<s.size()-1;i++){
+		 if(s[i]+1 != s[i+1]){
+			 cout<<"NO"<<endl;
+			 return;
+		 }
+	 }
+	 cout<<"YES"<<endl;
+
+}
+ 
+int32_t main()
+{
+	IOS;int t = 1;
+	cin>>t;
+	for(int i=1;i<=t;i++)
+	{
+		solve();
+	}
+}
