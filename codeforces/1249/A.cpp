6286a37b8cf14badc5fbe99f12e67cda37198258
Yash Gupta <y25.gupta@gmail.com>
2020-12-12 13:38:00 +0530

    Add solution for problem `A - Yet Another Dividing into Teams` Link: https://codeforces.com/contest/1249/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1249/submission/100998725

---
+++ codeforces/1249/A.cpp
@@ -0,0 +1,45 @@
+/*
+author: Yash Gupta
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(x) x.begin(),x.end()
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define mem(x,y) memset(x,y,sizeof(x))
+#define pb push_back
+#define mp make_pair
+#define fir first
+#define sec second
+const int N = 2e5 + 5;
+const int inf = 9e18 + 9;
+const int mod = 1e9+7;
+vector<int> a;
+
+void solve()
+{
+	 int n;cin>>n;
+	 vector<int> a(n);
+	 for(int i=0;i<n;i++){
+		 cin>>a[i];
+	 }
+	 sort(all(a));
+	 for(int i=0;i+1<n;i++){
+		 if(a[i]+1 == a[i+1]){
+			 cout<<2<<endl;
+			 return;
+		 }
+	 }
+	 cout<<1<<endl;
+}
+ 
+int32_t main()
+{
+	IOS;int t = 1;
+	cin>>t;
+	for(int i=1;i<=t;i++)
+	{
+		solve();
+	}
+}
