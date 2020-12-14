86699a3c73887b23e83cf0cfae84e9d7321ca777
Yash Gupta <y25.gupta@gmail.com>
2020-12-14 14:39:00 +0530

    Add solution for problem `A - Two Rabbits` Link: https://codeforces.com/contest/1304/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1304/submission/101154974

---
+++ codeforces/1304/A.cpp
@@ -0,0 +1,40 @@
+/*
+author: Yash Gupta
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(x) x.begin(),x.end()
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define mem(x,y) memset(x,y,sizeof(x))
+#define pb push_back
+#define mp make_pair
+#define fir first
+#define sec second
+const int N = 2e5 + 5;
+const int inf = 9e18 + 9;
+const int mod = 1e9+7;
+vector<int> a;
+
+void solve()
+{
+	 int x,y,a,b;cin>>x>>y>>a>>b;
+	 int n = (y-x);
+	 if(n%(a+b)){
+		 cout<<"-1"<<endl;
+	 }
+	 else{
+		 cout<<n/(a+b)<<endl;
+	 }
+}
+ 
+int32_t main()
+{
+	IOS;int t = 1;
+	cin>>t;
+	for(int i=1;i<=t;i++)
+	{
+		solve();
+	}
+}
