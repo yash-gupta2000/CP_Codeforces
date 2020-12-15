04eb63fd60d85635cc068b3ef6892e08d5380f6d
Yash Gupta <y25.gupta@gmail.com>
2020-12-15 21:57:00 +0530

    Add solution for problem `C - Unique Number` Link: https://codeforces.com/contest/1462/problem/C Tags: brute force, greedy, math, *900 Ref: https://codeforces.com/contest/1462/submission/101339742

--- codeforces/1462/C.cpp
+++ codeforces/1462/C.cpp
@@ -15,32 +15,78 @@ using namespace std;
 const int N = 2e5 + 5;
 const int inf = 9e18 + 9;
 const int mod = 1e9+7;
-vector<int> a;
+bool check(vector<int> temp){
+	set<int> t;
+	int sze = temp.size();
+	for(auto it:temp){
+		t.insert(it);
+	}
+	if(t.size()==sze){
+		return true;
+	}
+	return false;
+}
+void print_possible_change(vector<int> &change, int beginWith, int value, vector<vector<int>> &res, vector<int> &temp){
+	if(value == 0){
+		res.pb(temp);
+	}
 
+	for(int i = beginWith; i < change.size() && change[i] <= value; ++i){
+		temp.push_back(change[i]);
+		print_possible_change(change, i, value - change[i], res, temp);
+		temp.pop_back();
+	}
+}
+int stringTointeger(string str) 
+{ 
+    int temp = 0; 
+    for (int i = 0; i < str.length(); i++) { 
+        temp = temp * 10 + (str[i] - '0'); 
+    } 
+    return temp; 
+} 
 void solve()
 {
 	 int n;cin>>n;
-	 vector<int> res;
 	 if(n > 45){
 		 cout<<"-1"<<endl;
 		 return;
 	 }
-	 for(int i=9;i>=1;i--){
-		 if(n >= i){
-			 res.pb(i);
-			 n -= i;
+	 vector<int> a;
+	 for(int i=1;i<10;i++){
+		 a.pb(i);
+	 }
+	 vector<vector<int>> res;
+	 vector<int> curr;
+	 print_possible_change(a,0,n,res,curr);
+	 int mnsize = 100;
+	//  for(auto it:res){
+	// 	 for(auto x:it){
+	// 		 cout<<x<<" ";
+	// 	 }
+	// 	 cout<<endl;
+	//  }
+	 string ans="";
+	 vector<int> h;
+	 for(auto it:res){
+		 if(check(it)){
+			 string curr;
+			 for(auto x:it){
+				 curr += to_string(x);
+			 }
+			 int number = stringTointeger(curr);
+			 h.pb(number);
 		 }
 	 }
-	 if(n==0){
-		 sort(all(res));
-		 for(auto it:res){
-			 cout<<it;
-		 }
-		 cout<<endl;
+	 sort(all(h));
+	 if(h.size() > 0){
+		 cout<<h[0]<<endl;
 	 }
 	 else{
 		 cout<<"-1"<<endl;
 	 }
+
+
 }
  
 int32_t main()
