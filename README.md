7f28ffd004cb9ca986db0a5b9d7766a7ff55c561
Yash Gupta <y25.gupta@gmail.com>
2020-12-16 05:42:00 +0530

    Add solution for problem `E2 - Close Tuples (hard version)` Link: https://codeforces.com/contest/1462/problem/E2 Tags: binary search, combinatorics, implementation, math, sortings, two pointers, *1700 Ref: https://codeforces.com/contest/1462/submission/101373604

--- README.md
+++ README.md
@@ -12,5 +12,6 @@ A repository to keep track of problem solving practise, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-2 | [D - Divide and Summarize](https://codeforces.com/contest/1461/problem/D) | [GNU C++17](./codeforces/1461/D.cpp) | `binary search` `brute force` `data structures` `divide and conquer` `implementation` `sortings` `*1600` | Dec/17/2020 18:13 | 
-1 | [C - Unique Number](https://codeforces.com/contest/1462/problem/C) | [GNU C++17](./codeforces/1462/C.cpp) | `brute force` `greedy` `math` `*900` | Dec/16/2020 05:45 | 
+3 | [D - Divide and Summarize](https://codeforces.com/contest/1461/problem/D) | [GNU C++17](./codeforces/1461/D.cpp) | `binary search` `brute force` `data structures` `divide and conquer` `implementation` `sortings` `*1600` | Dec/17/2020 18:13 | 
+2 | [C - Unique Number](https://codeforces.com/contest/1462/problem/C) | [GNU C++17](./codeforces/1462/C.cpp) | `brute force` `greedy` `math` `*900` | Dec/16/2020 05:45 | 
+1 | [E2 - Close Tuples (hard version)](https://codeforces.com/contest/1462/problem/E2) | [GNU C++17](./codeforces/1462/E2.cpp) | `binary search` `combinatorics` `implementation` `math` `sortings` `two pointers` `*1700` | Dec/16/2020 05:42 | 
---
+++ codeforces/1462/E2.cpp
@@ -0,0 +1,112 @@
+/*
+author: Yash Gupta
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(x) x.begin(),x.end()
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define mem(x,y) memset(x,y,sizeof(x))
+#define pb push_back
+#define mp make_pair
+#define fir first
+#define sec second
+const int N = 2e5 + 5;
+const int inf = 9e18 + 9;
+const int MOD = 1e9+7;
+int fact[N], invfact[N];
+ 
+int pow(int a, int b, int m)
+{
+	int ans=1;
+	while(b)
+	{
+		if(b&1)
+			ans=(ans*a)%m;
+		b/=2;
+		a=(a*a)%m;
+	}
+	return ans;
+}
+ 
+int modinv(int k)
+{
+	return pow(k, MOD-2, MOD);
+}
+ 
+void precompute()
+{
+	fact[0]=fact[1]=1;
+	for(int i=2;i<N;i++)
+	{
+		fact[i]=fact[i-1]*i;
+		fact[i]%=MOD;
+	}
+	invfact[N-1]=modinv(fact[N-1]);
+	for(int i=N-2;i>=0;i--)
+	{
+		invfact[i]=invfact[i+1]*(i+1);
+		invfact[i]%=MOD;
+	}
+}
+ 
+int nCr(int x, int y)
+{
+	if(y>x)
+		return 0;
+	int num=fact[x];
+	num*=invfact[y];
+	num%=MOD;
+	num*=invfact[x-y];
+	num%=MOD;
+	return num;
+}
+
+void solve()
+{
+	 int n;cin>>n;
+	 int m,k;cin>>m>>k;
+	 vector<int> a(n);
+	 for(int i=0;i<n;i++){
+		 cin>>a[i];
+	 }
+	 sort(all(a));
+	 int res=0;
+	 for(int i=0;i<n;i++){
+		 int elmt = a[i];
+		 int req = elmt + k;
+		 int l=0,r=n-1;
+		 int idx = -1;
+		 while(r>=l){
+			 //cout<<l<<" "<<r<<endl;
+			 int m = (l+r)/2;
+			 if(a[m] <= req){
+				 idx = m;
+				 l = m+1;
+			 }
+			 else{
+				 r = m-1;
+			 }
+		 }
+		 l = i,r = idx;
+		 //cout<<l<<" "<<r<<endl;
+		 if(r-l+1 >= m && l<=r){
+			 int N=r-l+1-1;
+        res=(res+nCr(N,m-1))%MOD;
+		 }
+	 }
+	 cout<<res<<endl;
+
+}
+ 
+int32_t main()
+{
+	precompute();
+	IOS;int t = 1;
+	cin>>t;
+	for(int i=1;i<=t;i++)
+	{
+		solve();
+	}
+}
